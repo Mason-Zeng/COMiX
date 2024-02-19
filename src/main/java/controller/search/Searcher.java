7ebe43102820e4e330fcb4f76b91213d76c7b5a6
Ayden Dazo <aydend@optonline.net>
2024-02-19 17:45:53 -0500

    Added tests for PublicationSorter

--- src/main/java/controller/search/Searcher.java
+++ src/main/java/controller/search/Searcher.java
@@ -1,6 +1,8 @@
-package controller;
+package controller.search;
 
 import java.util.List;
+
+import controller.Comic;
 
 public interface Searcher {
     //TODO need to add data for searchData as a parameter
--- src/test/java/controller/sort/PublicationSorterTest.java
+++ src/test/java/controller/sort/PublicationSorterTest.java
@@ -1,18 +1,66 @@
 package controller.sort;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Comparator;
+
 import org.junit.jupiter.api.Test;
+
+import controller.Comic;
 
 public class PublicationSorterTest {
     
     @Test
-    public void testCompare(){
+    public void testCompareDiffDates1(){
         //Setup
-        
+        int expected = 1;
+
+        //TODO fix the constructor when it is finished
+        Comic comic1 = new Comic(/* Feb 2nd 2002*/);
+        Comic comic2 = new Comic(/* Feb 3rd 2005*/);
+
+        Comparator<Comic> comparer = new PublicationDateSorter();
         
         //Invoke
-        
+        int actual = comparer.compare(comic1, comic2);
         
         //Analyze
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testCompareDiffDates2() {
+        //Setup
+        int expected = -1;
+
+        //TODO fix the constructor when it is finished
+        Comic comic1 = new Comic(/* Mar 2nd 2020*/);
+        Comic comic2 = new Comic(/* Feb 3rd 2005*/);
+
+        Comparator<Comic> comparer = new PublicationDateSorter();
         
+        //Invoke
+        int actual = comparer.compare(comic1, comic2);
+        
+        //Analyze
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSameDate() {
+        //Setup
+        int expected = 1;
+
+        //TODO fix the constructor when it is finished
+        Comic comic1 = new Comic(/* Apr 23rd 1994*/);
+        Comic comic2 = new Comic(/* Apr 23rd 1994*/);
+
+        Comparator<Comic> comparer = new PublicationDateSorter();
+        
+        //Invoke
+        int actual = comparer.compare(comic1, comic2);
+        
+        //Analyze
+        assertEquals(expected, actual);
     }
 }
