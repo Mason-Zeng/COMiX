3b33c723260f55ac7529a418900f4c4c0ef7aa10
Ayden Dazo <aydend@optonline.net>
2024-02-19 16:55:22 -0500

    Default sorter implemented

--- src/main/java/controller/DefaultSorter.java
+++ src/main/java/controller/DefaultSorter.java
@@ -6,7 +6,20 @@ public class DefaultSorter implements Comparator<Comic> {
 
     @Override
     public int compare(Comic comic1, Comic comic2) {
-        throw new UnsupportedOperationException("Unimplemented method 'compare'");
+        String title1 = comic1.getTitle();
+        String title2 = comic2.getTitle();
+
+        int seriesCompare = title1.compareTo(title2);
+        int volumeCompare = comic1.getVolumeNumber() - comic2.getVolumeNumber();
+        int issueCompare = comic1.getIssueNumber() -  comic2.getIssueNumber();
+        
+        if (seriesCompare == 0) {
+            if (volumeCompare == 0) {
+                return issueCompare;
+            }
+            return volumeCompare;
+        }
+        return seriesCompare;
     }
     
 }
