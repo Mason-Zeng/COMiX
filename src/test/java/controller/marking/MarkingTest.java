f537428a2a29067954caaf06cf8ad3d684dd81a7
mz1498 <mz1498@rit.edu>
2024-02-24 02:42:37 -0500

    Added test case if grade is not 1-10

--- src/test/java/controller/marking/MarkingTest.java
+++ src/test/java/controller/marking/MarkingTest.java
@@ -1,7 +1,7 @@
 package controller.marking;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
@@ -11,6 +11,8 @@ import org.junit.jupiter.api.Test;
 import controller.Comic;
 
 public class MarkingTest{
+
+
     @Test
     public void testMarkingGrade1(){
         //Setup
@@ -28,4 +30,37 @@ public class MarkingTest{
         //Analyze
         assertEquals(expected, test);
     }
+
+    @Test
+    public void testMarkingGradeGreaterThan1(){
+        //Setup
+        BigDecimal value = new BigDecimal(6);
+        MarkImplement comic = new Comic(value);
+        comic = new Grade(comic, 4);
+        comic = new Slab(comic);
+        
+        //Invoke
+        BigDecimal test = comic.getValue();
+        test = test.setScale(2, RoundingMode.HALF_EVEN);
+        BigDecimal expected = new BigDecimal(7.22);
+        expected = expected.setScale(2, RoundingMode.HALF_EVEN);
+        
+        //Analyze
+        assertEquals(expected, test);
+    }
+
+    public void testMarkingInvalid(){
+        //Setup
+        BigDecimal value = new BigDecimal(6);
+        MarkImplement comic = new Comic(value);
+        comic = new Grade(comic, 0);
+        comic = new Slab(comic);
+        
+        //Invoke
+        BigDecimal test = comic.getValue();
+        
+        //Analyze
+        assertNull(test);
+    }
+    
 }
