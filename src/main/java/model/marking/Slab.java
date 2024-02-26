95a2c5c2674e9d55c089020ec24e1852cfed2259
mz1498 <mz1498@rit.edu>
2024-02-26 18:27:06 -0500

    added documentation to classes and methods for marking classes

--- src/main/java/model/marking/Slab.java
+++ src/main/java/model/marking/Slab.java
@@ -2,13 +2,19 @@ package model.marking;
 
 import java.math.BigDecimal;
 
-
+/**
+ * Concrete decorator class for marking a comic.
+ */
 public class Slab extends ComicDecorator{
     
     public Slab(Marking comic) {
         super(comic);
     }
 
+    /**
+     * The method in charge of calculating the new value of the comic after grading.
+     * Multiplies the value of the comic by 2.
+     */
     public BigDecimal getValue(){
         BigDecimal multiplier = new BigDecimal(2);
         BigDecimal newVal = comic.getValue().multiply(multiplier);
