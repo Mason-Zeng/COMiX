39dcaa357bb762287fbe9bee6f88acb5ad745ba3
Swapnil K Acharjee <ska1751@rit.edu>
2024-04-08 20:37:13 -0400

    created persistence data handler

---
+++ src/main/java/controller/persistence/PersistenceHandler.java
@@ -0,0 +1,50 @@
+package controller.persistence;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+
+import controller.CollectionManager;
+import controller.foreign.ForeignDataHandler;
+import model.hierarchy.Collection;
+import model.marking.Marking;
+
+public class PersistenceHandler {
+
+    public static final String savePath = ".data/users/";
+    
+    private static PersistenceHandler handler;
+
+    private PersistenceHandler() {}
+
+    public static PersistenceHandler getPersistenceHandler() {
+        if (handler == null) {
+            handler = new PersistenceHandler();
+        }
+        return handler;
+    }
+
+    public void saveUser(Collection collection) throws IOException {
+        String destination = savePath + collection.getUser() + ".json";
+        File file = new File(destination);
+        ForeignDataHandler fdhandler = ForeignDataHandler.getHandler();
+        fdhandler.exportData(file, collection.getIssues());
+    }
+
+    public Collection loadUser(String name) throws FileNotFoundException {
+        String destination = savePath + name + ".json";
+        File file = new File(destination);
+        ForeignDataHandler fdhandler = ForeignDataHandler.getHandler();
+        List<Marking> comics = fdhandler.importData(file);
+
+        Collection result = new Collection(name);
+        CollectionManager colManager = new CollectionManager(result);
+
+        for (Marking mark : comics) {
+            colManager.addIssue(mark);
+        }
+        return result;
+    }
+
+}
