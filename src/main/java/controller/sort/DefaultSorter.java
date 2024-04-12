360aab6bb11456a5905ab87dd2f886d69226239e
Swapnil K Acharjee <ska1751@rit.edu>
2024-04-12 14:40:36 -0400

    removed unused imports

--- src/main/java/controller/sort/DefaultSorter.java
+++ src/main/java/controller/sort/DefaultSorter.java
@@ -1,10 +1,6 @@
 package controller.sort;
 
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.Comparator;
-import java.util.List;
-import java.util.regex.Pattern;
 
 import model.marking.Marking;
 
