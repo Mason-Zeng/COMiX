553d405b23d5dccb54d55cde85b137bad275922a
Swapnil K Acharjee <ska1751@rit.edu>
2024-04-22 16:12:26 -0400

    added public constructor poggers

--- src/main/java/model/Character.java
+++ src/main/java/model/Character.java
@@ -3,6 +3,10 @@ package model;
 public class Character {
     private String name;
 
+    public Character(String name) {
+        this.name = name;
+    }
+
     public String getName() {
         return name;
     }
