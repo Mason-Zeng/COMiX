925ac7cdf65392c3aa089e2172d54929fd4f5570
Swapnil K Acharjee <ska1751@rit.edu>
2024-04-25 23:03:28 -0400

    fixed problems with caretaker

--- src/main/java/model/history/Caretaker.java
+++ src/main/java/model/history/Caretaker.java
@@ -10,22 +10,38 @@ public class Caretaker <T> {
     private int point;
 
     public Caretaker(Originator<T> originator) {
+        this.originator = originator;
         states = new ArrayList<>();
-        point = 0;
+        point = -1;
+    }
+
+    private void rebuildList() {
+        if (!(states.size() - 1 == point)) {
+            states = states.subList(0, point+1);
+        }
     }
 
     public void capture() {
-        states = states.subList(0, ++point);
-        states.add(originator.createMemento());
+        states.add(++point, originator.createMemento());
+        rebuildList();
     }
     
     public void undo() {
-        if (point == 0) return;
+        System.out.println("undo start " + point);
+        if (point < 0) return;
+        if (point == states.size() - 1) {
+            states.add(++point, originator.createMemento());
+        } else {
+            states.set(point, originator.createMemento());        
+        }
         originator.restore(states.get(--point));
+        System.out.println("undo end " + point);
     }
 
     public void redo() {
+        System.out.println("redo start " + point);
         if (point == states.size() - 1) return;
         originator.restore(states.get(++point));
+        System.out.println("redo end " + point);
     }
 }
